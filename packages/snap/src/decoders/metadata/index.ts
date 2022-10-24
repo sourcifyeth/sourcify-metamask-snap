f761a0b18496d9ad6b95b2d9b21782795e728013
Marco Castignoli <marco@castignoli.it>
2022-10-24 16:53:19 +0200

    use bytecode-utils to decode the contract's bytecode

--- packages/snap/src/decoders/metadata/index.ts
+++ packages/snap/src/decoders/metadata/index.ts
@@ -1,5 +1,4 @@
-import { decode as cborDecode } from 'cbor-x';
-import bs58 from 'bs58';
+import { decode } from '@marcocastignoli/bytecode-utils';
 import { Transaction } from '../../utils';
 
 export const metadataDecoder = async (tx: Transaction): Promise<any | null> => {
@@ -12,21 +11,14 @@ export const metadataDecoder = async (tx: Transaction): Promise<any | null> => {
     return null;
   }
 
-  const bytecode = bytecodeRaw.toString();
-
-  const ipfsHashLength = parseInt(`${bytecode.slice(-4)}`, 16);
-  const cborEncoded = bytecode.substring(
-    bytecode.length - 4 - ipfsHashLength * 2,
-    bytecode.length - 4,
-  );
-
-  const contractMetadata = cborDecode(Buffer.from(cborEncoded, 'hex'));
+  const decodedObject = decode(bytecodeRaw as string);
+  if (!decodedObject.ipfs) {
+    return null;
+  }
 
   let contractMetadataJSON: any;
   try {
-    const req = await fetch(
-      `https://ipfs.io/ipfs/${bs58.encode(contractMetadata.ipfs)}`,
-    );
+    const req = await fetch(`https://ipfs.io/ipfs/${decodedObject.ipfs}`);
     contractMetadataJSON = await req.json();
   } catch (e) {
     console.log(e);
